1f8e681004d8ae4d74eb1c7639cbe9a7c6294012
Mark R. Tuttle <mrtuttle@amazon.com>
2019-08-25 05:53:30 -0400

    Clean up the html markup of source files.
    Still to do include cleaning up all the little utility functions.

--- scripts/cbmc-viewer/markup.py
+++ scripts/cbmc-viewer/markup.py
@@ -1,6 +1,5 @@
 """Mark up the source tree and links into the source tree."""
 
-import subprocess
 import sys
 import html
 import os
@@ -29,24 +28,20 @@ class Markup:
         """Map a source file path to the corresponding html file path."""
         return src+".html"
 
-    def link_to_line(self, text, src, line, depth=0,
-                     color=None, target=None):
+    def link_line(self, src, line, text, depth=0, target=None):
         """Link text to a line in a source file."""
-        # pylint: disable=too-many-arguments
-        color = None if color == 'black' else color
-        color = None if not color else color
-        style = ' style="color:{}"'.format(color) if color else ''
+
         path = directory_ancestor(depth) + '/' + self.src_html(src)
         line = '#{}'.format(line) if line else ''
         tgt = ' target="{}"'.format(target) if target else ''
-        return '<a{} href="{}{}"{}>{}</a>'.format(style, path, line, tgt, text)
+        return '<a href="{}{}"{}>{}</a>'.format(path, line, tgt, text)
 
-    def link_to_file(self, text, src, depth=0, color=None, target=None):
+    def link_to_file(self, text, src, depth=0, target=None):
         """Link text to a source file."""
         # pylint: disable=too-many-arguments
-        return self.link_to_line(text, src, 0, depth, color, target)
+        return self.link_line(src, 0, text, depth, target)
 
-    def link_symbol(self, symbol, depth=0, color=None, target=None, text=None):
+    def link_symbol(self, symbol, depth=0, target=None, text=None):
         """Link a symbol to its definition."""
         # pylint: disable=too-many-arguments
         val = self.symbols.lookup(symbol)
@@ -55,7 +50,7 @@ class Markup:
 
         text = text or symbol
         (src, line) = val
-        return self.link_to_line(text, src, line, depth, color, target)
+        return self.link_line(src, line, text, depth, target)
 
     def link_file(self, src, depth=0, color=None):
         """Link a source file to its html file."""
@@ -65,7 +60,7 @@ class Markup:
         """Link a function to its definition."""
         return self.link_symbol(function, depth, color)
 
-    def markup_source_location(self, loc, depth=0, color=None, target=None):
+    def markup_source_location(self, loc, depth=0, target=None):
         # pylint: disable=too-many-locals
         """Link trace source location to file, line, and function."""
 
@@ -79,21 +74,20 @@ class Markup:
         file_text_start = match.start(1)
         file_text_end = match.end(1)
         file_name = match.group(2)
-        file_link = self.link_to_file(file_text, file_name,
-                                      depth, color, target)
+        file_link = self.link_to_file(file_name, depth, file_text, target)
 
         func_text = match.group(3)
         func_text_start = match.start(3)
         func_text_end = match.end(3)
         func_name = match.group(4)
-        func_link = self.link_symbol(func_name, depth, color, target, func_text)
+        func_link = self.link_symbol(func_name, depth, target, func_text)
 
         line_text = match.group(5)
         line_text_start = match.start(5)
         line_text_end = match.end(5)
         line_num = match.group(6)
-        line_link = self.link_to_line(line_text, file_name, line_num,
-                                      depth, color, target)
+        line_link = self.link_line(file_name, line_num, line_text,
+                                   depth, target)
 
         if file_name[0] == '<' and file_name[-1] == '>':
             return html.escape(loc, quote=False)
@@ -103,74 +97,57 @@ class Markup:
                 loc[func_text_end:line_text_start] + line_link +
                 loc[line_text_end:])
 
-    def markup_file(self, indir, infile, outfile, depth, coverage):
-        """Mark up a source file with links to symbol definitions."""
-        # pylint: disable=too-many-arguments
-        try:
-            # Some files on Windows appear to use an encoding
-            # incompatible with the Python 3 default utf-8.  Python 2
-            # used to assume latin-1.  The utf-8 and latin-1 encodings
-            # agree on ascii.
-            infh = open(infile, "r", encoding="latin-1")
-        except IOError as e:
-            print("Unable to open {} for reading: {}".format(infile,
-                                                             e.strerror))
-            sys.exit()
-        try:
-            os.makedirs(os.path.dirname(outfile))
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        try:
-            outfh = open(outfile, "w", encoding="latin-1")
-        except IOError as e:
-            print("Unable to open {} for writing: {}".format(outfile,
-                                                             e.strerror))
-            sys.exit()
+    @staticmethod
+    def read_source_file(srcfile):
+        """Read source file contents and clean up whitepsace."""
 
-        # Strip source directory from source filename for the html page title
-        filename = infile
-        if filename.startswith(indir):
-            filename = filename[len(indir):]
-            filename = filename.lstrip('/')
+        # Read file using latin-1 encode.  Some files created on
+        # Windows in Europe appear to use latin-1 and not ascii or
+        # utf-8.  All three are compatible on the ascii character set.
+        # But remember that Python latin-1 is slightly different from
+        # what Windows calls latin-1.
+        with open(srcfile, encoding="latin-1") as handle:
+            lines = handle.read().splitlines()
+            lines = [untabify(line).rstrip() for line in lines]
+            return '\n'.join(lines)
 
-        contents = html.escape(infh.read(), quote=False)
-        segments = comment.parse(contents)
-        def markup_symbol(symbol, depth):
-            val = self.symbols.lookup(symbol)
-            if not val:
-                return symbol
-            (src, line) = val
-            return '<a href="{}/{}#{}">{}</a>'.format(
-                directory_ancestor(depth), self.src_html(src), line, symbol)
+    def write_html_file(self, title, contents, htmlfile, depth, coverage):
+        """Write soure file contents as html annotated with coverage."""
+
         def markup_segment(segment, depth):
             if not comment.code_start(segment):
                 return segment
             tokens = re.split('([_a-zA-Z][_a-zA-Z0-9]*)', segment)
-            return "".join([markup_symbol(token, depth) for token in tokens])
-        markedup_segments = [markup_segment(segment, depth)
-                             for segment in segments]
-        markedup_contents = "".join(markedup_segments).splitlines()
+            return "".join([self.link_symbol(token, depth) for token in tokens])
 
-        html_header(outfh, filename, depth)
-        lineno = 0
-        for line in markedup_contents:
-            lineno += 1
-            ls = coverage.get(lineno) if coverage else None
-            line = untabify(line).rstrip()
-            try:
-                html_line(outfh, line, lineno, ls)
-            except UnicodeEncodeError:
-                print("UnicodeEncodeError:")
-                print("file: {}".format(infile))
-                print("line number: {}".format(lineno))
-                print("line:")
-                print(line)
-                print("Skipping this line and continuing...")
-        html_footer(outfh)
+        contents = html.escape(contents, quote=False)
+        segments = comment.parse(contents)
+        segments = [markup_segment(segment, depth) for segment in segments]
+        contents = ''.join(segments)
 
-        infh.close()
-        outfh.close()
+        try:
+            os.makedirs(os.path.dirname(htmlfile))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        coverage = coverage or {}
+        with open(htmlfile, "w") as handle:
+            html_header(handle, title, depth)
+            lineno = 0
+            for line in contents.splitlines():
+                lineno += 1
+                html_line(handle, line, lineno, coverage.get(lineno))
+            html_footer(handle)
+
+    def markup_file(self, indir, infile, outfile, depth, coverage):
+        """Mark up a source file with links to symbol definitions."""
+
+        contents = self.read_source_file(infile)
+        title = infile
+        if title.startswith(indir):
+            title = title[len(indir):].lstrip(os.sep)
+        self.write_html_file(title, contents, outfile, depth, coverage)
 
     def markup_directory(self):
         """Mark up a source directory with links to symbol definitions."""
@@ -191,8 +168,8 @@ class Markup:
             if src[0] == '/':
                 continue
             self.markup_file(self.srcdir,
-                             self.srcdir+'/'+src,
-                             self.htmldir+'/'+self.src_html(src),
+                             os.path.join(self.srcdir, src),
+                             os.path.join(self.htmldir, self.src_html(src)),
                              directory_depth(src),
                              self.coverage.line.get(src))
 
@@ -231,52 +208,6 @@ def directory_ancestor(depth):
     if depth < 1:
         return '.'
     return '/'.join(['..' for n in range(depth)])
-
-################################################################
-
-
-def source_files(srcdir, srcfilter=""):
-    """Source files appearing in a source directory.
-
-    Args:
-        srcdir (str): the root of the source directory.
-        srcfilter (str): a python regular expression for files to ignore.
-    """
-    cwd = os.getcwd()
-
-    files = []
-
-    try:
-        os.chdir(srcdir)
-    except OSError as e:
-        msg = "Can't annotate source files: "
-        msg += "Can't change to directory {}: {}".format(srcdir, e.strerror)
-        print(msg)
-        return []
-
-    cmd = ["find", "-L", ".", "(", "-iname", "*.[ch]", "-or", "-iname", "*.inl", ")"]
-    try:
-        find = subprocess.check_output(cmd).decode('utf-8')
-    except:
-        msg = "Can't annotate source files: "
-        msg += "Can't run command '{}'".format(" ".join(cmd))
-        print(msg)
-        raise
-
-    files = find.split('\n')
-    if srcfilter:
-        files = [f for f in files if not re.match(srcfilter, f)]
-    files = [re.sub('^./', '', f) for f in files]
-
-    try:
-        os.chdir(cwd)
-    except OSError as e:
-        msg = "Can't annotate source files: "
-        msg += "Can't change to directory {}: {}".format(cwd, e.strerror)
-        print(msg)
-        sys.exit()  # don't just return
-
-    return files
 
 ################################################################
 
