24274c529e84082d7ee5e623d038bc28a5dbfbc5
Felipe R. Monteiro <felisous@amazon.com>
2020-02-03 19:26:55 -0500

    Adds new version of the status report generator
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- scripts/cbmc-viewer/viewer/metrics/report_generator.py
+++ scripts/cbmc-viewer/viewer/metrics/report_generator.py
@@ -7,6 +7,7 @@ import os                                 # get unix
 import json
 from   glob    import glob                # file name expansion
 import pandas as pd
+import logging                            # setup log files
 
 def chartGenerator():
     root = os.getcwd()
@@ -14,16 +15,22 @@ def chartGenerator():
 
     for harness in glob(root + '/*'):      # for all harnesses
         if (os.path.isdir(harness)):
-            with open(harness + '/viewer-summary.json', 'r') as f:
-                viewer_summary = f.read()
-            viewer_dict = json.loads(viewer_summary)
-            report['Proof'][os.path.basename(harness)] = os.path.basename(harness)
-            report['Model LOC'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['model-lines'])
-            report['Project LOC'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['project-lines'])
-            report['Project Coverage'][os.path.basename(harness)] = round(float(len(viewer_dict[os.path.basename(harness)]['project-lines-hit']))/float(len(viewer_dict[os.path.basename(harness)]['project-lines'])), 2)
-            report['Property Issues'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['property-issue-lines'])
-            report['Loop Issues'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['loop-issue-lines'])
-            report['Other Issues'][os.path.basename(harness)] = viewer_dict[os.path.basename(harness)]['other-issues-count']
+            try:
+                with open(harness + '/viewer-summary.json', 'r') as f:
+                    viewer_summary = f.read()
+                viewer_dict = json.loads(viewer_summary)
+                report['Proof'][os.path.basename(harness)] = os.path.basename(harness)
+                report['Model LOC'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['model-lines'])
+                report['Project LOC'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['project-lines'])
+                report['Project Coverage'][os.path.basename(harness)] = str(round(float(len(viewer_dict[os.path.basename(harness)]['project-lines-hit']))/float(len(viewer_dict[os.path.basename(harness)]['project-lines'])), 2)*100)+"%"
+                report['Property Issues'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['property-issue-lines'])
+                report['Loop Issues'][os.path.basename(harness)] = len(viewer_dict[os.path.basename(harness)]['loop-issue-lines'])
+                report['Other Issues'][os.path.basename(harness)] = viewer_dict[os.path.basename(harness)]['other-issues-count']
+                report['SAT Variables'][os.path.basename(harness)] = viewer_dict[os.path.basename(harness)]['sat-variables']
+                report['SAT Clauses'][os.path.basename(harness)] = viewer_dict[os.path.basename(harness)]['sat-clauses']
+                report['SAT Time'][os.path.basename(harness)] = str(round(float(viewer_dict[os.path.basename(harness)]['sat-time']), 5)) + "s"
+            except IOError:
+                logging.warning(os.path.basename(harness) + ' does not have a viewer-summary.json file.')
 
     with open('summary.json', 'r') as f:
         viewer_summary = f.read()
@@ -32,16 +39,20 @@ def chartGenerator():
     report['Proof']['TOTAL'] = "TOTAL"
     report['Model LOC']['TOTAL'] = len(summary['summary']['model-lines'])
     report['Project LOC']['TOTAL'] = len(summary['summary']['project-lines'])
-    report['Project Coverage']['TOTAL'] = round(float(len(summary['summary']['project-lines-hit']))/float(len(summary['summary']['project-lines'])), 2)
+    report['Project Coverage']['TOTAL'] = str((round(float(len(summary['summary']['project-lines-hit']))/float(len(summary['summary']['project-lines'])), 2))*100)+"%"
     report['Property Issues']['TOTAL'] = len(summary['summary']['property-issue-lines'])
     report['Loop Issues']['TOTAL'] = len(summary['summary']['loop-issue-lines'])
     report['Other Issues']['TOTAL'] = summary['summary']['other-issues-count']
+    report['SAT Variables']['TOTAL'] = "--"
+    report['SAT Clauses']['TOTAL'] = "--"
+    report['SAT Time']['TOTAL'] = "--"
 
     with open('report.json', 'w', encoding='utf-8') as f:
         json.dump(report, f, ensure_ascii=False, indent=4)
 
     df = pd.read_json (r'report.json')
-    df.to_csv (r'report.csv', index = None)
+    df.to_csv (r'MQTT Proof Report.csv', index = None)
+    print("Report generated successfully!")
 
 def summaryGenerator():
     root = os.getcwd()
@@ -49,34 +60,40 @@ def summaryGenerator():
 
     for harness in glob(root + '/*'):      # for all harnesses
         if (os.path.isdir(harness)):
-            with open(harness + '/viewer-summary.json', 'r') as f:
-                viewer_summary = f.read()
-            viewer_dict = json.loads(viewer_summary)
-            if len(summary['summary']) == 0:
-                summary['summary']['model-lines'] = viewer_dict[os.path.basename(harness)]['model-lines']
-                summary['summary']['project-lines'] = viewer_dict[os.path.basename(harness)]['project-lines']
-                summary['summary']['project-lines-hit'] = viewer_dict[os.path.basename(harness)]['project-lines-hit']
-                summary['summary']['property-issue-lines'] = viewer_dict[os.path.basename(harness)]['property-issue-lines']
-                summary['summary']['loop-issue-lines'] = viewer_dict[os.path.basename(harness)]['loop-issue-lines']
-                summary['summary']['other-issues-count'] = viewer_dict[os.path.basename(harness)]['other-issues-count']
-                summary['summary']['expected-missing-funcs'] = viewer_dict[os.path.basename(harness)]['expected-missing-funcs']
-                summary['summary']['unexpected-missing-funcs'] = viewer_dict[os.path.basename(harness)]['unexpected-missing-funcs']
-            else:
-                summary['summary']['model-lines'] += viewer_dict[os.path.basename(harness)]['model-lines']
-                summary['summary']['project-lines'] += viewer_dict[os.path.basename(harness)]['project-lines']
-                summary['summary']['project-lines-hit'] += viewer_dict[os.path.basename(harness)]['project-lines-hit']
-                summary['summary']['property-issue-lines'] += viewer_dict[os.path.basename(harness)]['property-issue-lines']
-                summary['summary']['loop-issue-lines'] += viewer_dict[os.path.basename(harness)]['loop-issue-lines']
-                summary['summary']['other-issues-count'] += viewer_dict[os.path.basename(harness)]['other-issues-count']
-                summary['summary']['expected-missing-funcs'] += viewer_dict[os.path.basename(harness)]['expected-missing-funcs']
-                summary['summary']['unexpected-missing-funcs'] += viewer_dict[os.path.basename(harness)]['unexpected-missing-funcs']
-
+            try:
+                with open(harness + '/viewer-summary.json', 'r') as f:
+                    viewer_summary = f.read()
+                viewer_dict = json.loads(viewer_summary)
+                if len(summary['summary']) == 0:
+                    summary['summary']['model-lines'] = viewer_dict[os.path.basename(harness)]['model-lines']
+                    summary['summary']['project-lines'] = viewer_dict[os.path.basename(harness)]['project-lines']
+                    summary['summary']['project-lines-hit'] = viewer_dict[os.path.basename(harness)]['project-lines-hit']
+                    summary['summary']['property-issue-lines'] = viewer_dict[os.path.basename(harness)]['property-issue-lines']
+                    summary['summary']['loop-issue-lines'] = viewer_dict[os.path.basename(harness)]['loop-issue-lines']
+                    summary['summary']['other-issues-count'] = viewer_dict[os.path.basename(harness)]['other-issues-count']
+                    summary['summary']['expected-missing-funcs'] = viewer_dict[os.path.basename(harness)]['expected-missing-funcs']
+                    summary['summary']['unexpected-missing-funcs'] = viewer_dict[os.path.basename(harness)]['unexpected-missing-funcs']
+                else:
+                    summary['summary']['model-lines'] += viewer_dict[os.path.basename(harness)]['model-lines']
+                    summary['summary']['project-lines'] += viewer_dict[os.path.basename(harness)]['project-lines']
+                    summary['summary']['project-lines-hit'] += viewer_dict[os.path.basename(harness)]['project-lines-hit']
+                    summary['summary']['property-issue-lines'] += viewer_dict[os.path.basename(harness)]['property-issue-lines']
+                    summary['summary']['loop-issue-lines'] += viewer_dict[os.path.basename(harness)]['loop-issue-lines']
+                    summary['summary']['other-issues-count'] += viewer_dict[os.path.basename(harness)]['other-issues-count']
+                    summary['summary']['expected-missing-funcs'] += viewer_dict[os.path.basename(harness)]['expected-missing-funcs']
+                    summary['summary']['unexpected-missing-funcs'] += viewer_dict[os.path.basename(harness)]['unexpected-missing-funcs']
+            except IOError:
+                logging.warning(os.path.basename(harness) + ' does not have a viewer-summary.json file.')
 
 
     with open('summary.json', 'w', encoding='utf-8') as f:
         json.dump(summary, f, ensure_ascii=False, indent=4)
+    print("Summary generated successfully!")
 
 def main():
+    logging.basicConfig(filename='report.log',
+                        filemode='w',
+                        format='%(name)s - %(levelname)s - %(message)s')
     summaryGenerator()
     chartGenerator()
 
