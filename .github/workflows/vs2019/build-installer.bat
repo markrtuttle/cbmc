82e3d0529887af59a443cda8481f6f3a414660ee
Mark R Tuttle <mrtuttle@amazon.com>
2020-05-18 17:18:38 +0000

    Fix up stable/latest vs2019 installers

--- .github/workflows/vs2019/build-installer.bat
+++ .github/workflows/vs2019/build-installer.bat
@@ -2,21 +2,23 @@
 
 echo on
 
+@REM PGK can have values cbmc or cbmc-latest
 set PKG=%1
-set NAME=cbmc-latest
 set SCRIPTDIR=.github\workflows\vs2019
+
+if not "%PKG%"=="cbmc" if not "%PKG%"=="cbmc-latest" (
+  echo PKG must be "cbmc" or "cbmc-latest", found "%PKG%"
+  exit 1
+)
 
 echo "Set up PATH to find wix"
 PATH=%PATH%;"C:\Program Files (x86)\WiX Toolset v3.11\bin"
 
 echo "Build installer: run candle"
-candle -o %NAME%.wixobj -arch x64 %SCRIPTDIR%\%NAME%.wxs
+candle -o %PKG%.wixobj -arch x64 %SCRIPTDIR%\%PKG%.wxs
 
 echo "Build installer: run light"
-light -o %PKG% -ext WixUIExtension %NAME%.wixobj
-
-echo "Package is %PKG%"
-echo %PKG%
+light -o %PKG%.msi -ext WixUIExtension %PKG%.wixobj
 
 echo %cd%
 dir %cd%
